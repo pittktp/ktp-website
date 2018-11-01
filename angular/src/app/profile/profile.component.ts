5272c39de2c3e05de527f352ffec976b7116fb62
Zvonty Flugence <zvontyflugence@gmail.com>
2018-11-01 17:41:23 -0400

    Added Settings Modal and Fixed failed profile init on logout

--- angular/src/app/profile/profile.component.ts
+++ angular/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { AuthService } from '../auth.service';
@@ -6,7 +7,7 @@ import { MemberService } from '../shared/api/member.service';
 import { RequestsService } from '../shared/api/requests.service';
 import { SharedService } from '../shared.service';
 import { Member } from '../shared/models/member.model';
-import { Request } from '../shared/models/request.model';
+import { PictureRequest } from '../shared/models/pictureRequest.model';
 import { ToastrService } from 'ngx-toastr';
 
 import '../../assets/js/new-age.min.js';
@@ -18,22 +19,20 @@ declare var $: any;
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit, OnDestroy {
+export class ProfileComponent implements OnInit {
 
   id: number
   profile: Member
   private sub: any
 
   constructor(private toastr: ToastrService, private auth: AuthService, private memberService: MemberService, private requestService: RequestsService, private router: Router, private route: ActivatedRoute, private sharedService: SharedService) {
-    if(!this.auth.loggedIn()) {
-      this.router.navigate(['home']);
-    }
+
   }
 
   ngOnInit() {
     this.loadScript('../assets/js/new-age.js');
 
-    this.sub = this.route.params.forEach(params => {
+    this.route.params.forEach(params => {
       this.id = +params['id'];
 
       // TODO: Change to its own API call for simplicity
@@ -56,7 +55,17 @@ export class ProfileComponent implements OnInit, OnDestroy {
     script.src = src;
   }
 
-  ngOnDestroy() {
-    this.sub.unsubscribe();
+  
+  onChangePictureSubmit(form: NgForm) {
+    var request = new PictureRequest();
+    request.filename = form.value.picture.split("\\").pop();
+    request.submittedById = this.auth.getCurrentUserId();
+    // Save picture in assets/img/profiles folder
+
+    // Store filename in DB
+    // this.requestService.postPictureRequest(request).subscribe(res => {
+    //   $("#profileSettingsModal").modal("hide");
+    //   this.toastr.success('Profile Picture has been updated');
+    // });
   }
 }
