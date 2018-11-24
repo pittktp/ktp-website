5fedc458f965dce87957c931206a385516cf4901
Jeremy Deppen <jmd221@pitt.edu>
2018-11-24 10:40:16 -0500

    Edited Dockerfile and nginx.conf to allow https hosting

--- server/controllers/memberController.js
+++ server/controllers/memberController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 var router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
 
