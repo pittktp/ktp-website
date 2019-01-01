cb6cde947cfd906fdf6dbef9c0158a43e32b38ef
Jeremy Deppen <jmd221@pitt.edu>
2019-01-01 08:31:30 -0700

    Whoooops

--- angular/src/app/shared/api/requests.service.ts
+++ angular/src/app/shared/api/requests.service.ts
@@ -10,11 +10,11 @@ import { Request } from '../models/request.model';
 export class RequestsService {
 
   // Test - use this url when running locally on your own computer
-  API_URL = 'http://localhost:3000/api/requests/';
+  //API_URL = 'http://localhost:3000/api/requests/';
 
   // Production - use this url when running in production on AWS
   //API_URL = 'http://ec2-54-157-59-182.compute-1.amazonaws.com:3000/api/requests/';
-  //API_URL = 'https://pitt-kappathetapi.com/api/requests/';
+  API_URL = 'https://pitt-kappathetapi.com/api/requests/';
 
   requests: Request[];
 
